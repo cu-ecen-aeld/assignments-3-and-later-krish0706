bdcaeb3085dde56f9ed5880e9a1e121d6cd8a613
kshah <krishsowmilshah@gmail.com>
2025-01-19 19:48:12 -0700

    Adds comments

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
+# @file finder.sh
+# @author krish shah
+# @brief Implements a shell script to search all the files in the given 
+# directory (filesdir) for the (searchstr). It then prints out the number 
+# of files in the directory and all subdirectories and the number of lines
+# which contain the (searchstr). This script will return 1 with error if 
+# 2 arguments are not specified, or if filesdir is not a valid directory
+# Usage: 
+# ./finder.sh [filesdir] [searchstr]
 
+# check if 2 positional arguments are provided to the
+# script, if not exit with return value 1
 if [ $# -ne 2 ]; 
 then
-    echo 'ERROR: invalid number of arguements'
+    echo 'ERROR: invalid number of arguments'
     echo 'EXPECTED USAGE: ./finder.sh [filesdir] [searchstr]'
     exit 1
 fi
@@ -10,13 +21,22 @@ fi
 filesdir=$1
 searchstr=$2
 
+# if filesdir is not a valid directory, exit with return 
+# value 1
 if [ ! -d $filesdir ];
 then 
     echo "$filesdir is not a valid directory!"
     exit 1
 fi
 
+# list all files in the filesdir using the find command 
+# and pipe the output to wc to count the number of lines
+# (each file will occupy one line)
 X=$(find "$filesdir" -type f  | wc -l)
+
+# grep filesdir for searchstr, and suppress error messages
+# pipe the output to wc to count the number of lines
+# (grep will output each line where searchstr is found)
 Y=$(grep -r -s $searchstr $filesdir | wc -l)
 
 echo "The number of files are ${X} and the number of matching lines are ${Y}"
--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
+# @file writer.sh
+# @author krish shah
+# @brief Implements a shell script to write the (writestr) to the 
+# (writefile), the writefile is the full path of the file on the 
+# filesystem. This script creates a new file and path if it does 
+# not exist or overwrites the existing file. This script will 
+# return 1 with error if 2 arguments are not specified, or if
+# it is unable to create the file
+# Usage:
+# ./write.sh [writefile] [writestr]
 
-
+# check if 2 positional arguments are provided to the
+# script, if not exit with return value 1
 if [ $# -ne 2 ]; 
 then
     echo 'ERROR: invalid number of arguements'
@@ -11,8 +22,10 @@ fi
 writefile=$1
 writestr=$2
 
+# use dirname to strip the filename from the file path
 writefilepath=$(dirname $writefile)
 
+# if writefilepath is not a valid directory, create it
 if [ ! -d $writefilepath ]; 
 then 
     mkdir -p $writefilepath
@@ -20,6 +33,7 @@ fi
 
 echo "${writestr}" > $writefile
 
+# if writefile was not created, exit with return value 1
 if [ $? -ne 0 ]; then
     echo "ERROR: file could not be created!"
     exit 1
